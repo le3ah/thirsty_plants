381d3ebecbafd284a9c955ffd513b2ccd5d3d1cb
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-02-12 10:27:25 -0700

    Adds feature test for seeing a new garden form.
    Co-authored-by: Anna Smolentzov 
    6686861+asmolentzov@users.noreply.github.com

---
+++ spec/features/user_creates_garden_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe 'As a logged in user on the site' do
+  it 'Can see a new garden form' do
+    user = User.create(name: "User1", email: "user@example.com")
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit dashboard_path
+    click_on 'Create Garden'
+
+    expect(current_path).to eq(new_garden_path)
+    expect(page).to have_field("Garden Name")
+    expect(page).to have_field("Garden Zip Code")
+    expect(page).to have_field("Plant Name")
+    expect(page).to have_field("Plant Watering Requirement")
+  end
+end

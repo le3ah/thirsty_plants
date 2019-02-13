54ad5060aa111dfd27466bccc7384c4257d97bb7
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-12 21:35:24 -0700

    Adds error checking for plant creation

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -6,10 +6,15 @@ class PlantsController < ApplicationController
   end
   
   def create
-    garden = Garden.find(params[:garden_id])
-    garden.plants.create(plant_params)
-    flash[:success] = "Party in my plants, you've created a new plant!"
-    redirect_to garden_path(garden)
+    @garden = Garden.find(params[:garden_id])
+    @plant = @garden.plants.new(plant_params)
+    if @plant.save
+      flash[:success] = "Party in my plants, you've created a new plant!"
+      redirect_to garden_path(@garden)
+    else
+      @errors = @plant.errors
+      render :new
+    end
   end
   
   private

6a7834ef80d72a1893f38f8cd09abc9f426c8e69
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-12 21:28:08 -0700

    Adds feature test for creating plants with no info

--- spec/features/user_can_add_plant_to_garden_spec.rb
+++ spec/features/user_can_add_plant_to_garden_spec.rb
@@ -23,5 +23,17 @@ describe 'As a logged in user to the site' do
         expect(page).to have_content("Watering Requirements: 5 times/week")
       end
     end
+    
+    it 'cannot add a plant with incomplete information' do
+      user = create(:user)
+      garden = create(:garden, user: user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      
+      visit new_garden_plant_path(garden)
+      click_button 'Create Plant'
+      
+      expect(page).to have_content("Name can't be blank")
+      expect(page).to have_content("Times per week can't be blank")
+    end
   end
 end

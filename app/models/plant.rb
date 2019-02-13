ca06337afd3a23b315ed9c30f310f1866939f278
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-02-12 18:13:03 -0700

    Adds model validations for plant.
    Co-authored-by: Anna Smolentzov 
    <6686861+asmolentzov@users.noreply.github.com>

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,3 +1,5 @@
 class Plant < ApplicationRecord
   belongs_to :garden
+  validates_presence_of :name
+  validates_presence_of :times_per_week
 end
--- spec/models/plant_spec.rb
+++ spec/models/plant_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Plant, type: :model do
-  
+  describe 'Validations' do
+    it { should validate_presence_of :name}
+    it { should validate_presence_of :times_per_week}
+  end
+
+  describe 'Relationships' do
+    it { should belong_to :garden }
+  end
 end

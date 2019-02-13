0c91020ed9eda163df2173adec62cb0e362d0015
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-12 20:40:55 -0700

    Adds feature test for creating a new plant

---
+++ spec/features/user_can_add_plant_to_garden_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe 'As a logged in user to the site' do
+  describe 'on a garden show page' do
+    it 'can add a new plant to their garden' do
+      user = create(:user)
+      garden = create(:garden, user: user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      
+      visit garden_path(garden)
+      
+      click_on 'Add a Plant to Your Garden!'
+      
+      expect(path).to eq(new_plant_path)
+      fill_in :plant_name, with: 'Roses'
+      fill_in :plant_times_per_week, with: 5
+      click_button 'Create Plant'
+      
+      expect(current_path).to eq(garden_path(garden))
+      expect(page).to have_content("Party in my plants, you've created a new plant!")
+      within '.plants' do
+        expect(page).to have_content('Roses')
+        expect(page).to have_content("Watering Requirements: 5 times/week")
+      end
+    end
+  end
+end

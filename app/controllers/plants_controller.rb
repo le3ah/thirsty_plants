1e2e0adf9b4c962f41da90ed35fd7c4fbea60f0e
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-12 21:10:01 -0700

    Adds creating plant

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -7,8 +7,14 @@ class PlantsController < ApplicationController
   
   def create
     garden = Garden.find(params[:garden_id])
-    
+    garden.plants.create(plant_params)
     flash[:success] = "Party in my plants, you've created a new plant!"
     redirect_to garden_path(garden)
+  end
+  
+  private
+  
+  def plant_params
+    params.require(:plant).permit(:name, :times_per_week)
   end
 end

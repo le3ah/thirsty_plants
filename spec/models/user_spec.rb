13291e692918d6aefb7f169be15ea9db1a7f5f53
Leah Miller <lkmiller.87@gmail.com>
2019-02-13 08:32:32 -0700

    Adds attribute tests for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,23 +1,13 @@
 require 'rails_helper'
 
-describe User do
-  it "exists" do
-    attributes = {}
-    user = User.new(attributes)
-    expect(user).to be_a(User)
-  end
-  it "has attributes" do
-    attributes = { first_name: "Kathy",
-                   last_name: "Miller",
-                   email: "mom@gmail.com",
-                   google_token: "itsastring",
-                   google_id_token: "itsalsoastring"}
-    user = User.new(attributes)
-    expect(user.first_name).to eq("Kathy")
-    expect(user.last_name).to eq("Miller")
-    expect(user.email).to eq("mom@gmail.com")
-    expect(user.google_token).to eq("itsastring")
-    expect(user.google_id_token).to eq("itsalsoastring")
 
+RSpec.describe User, type: :model do
+  describe 'validations' do
+  it { should validate_presence_of :first_name }
+  it { should validate_presence_of :last_name }
+  it { should validate_presence_of :email }
+  it { should validate_presence_of :google_token }
+  it { should validate_presence_of :google_id_token }
   end
+
 end

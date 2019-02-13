2e2c493ea4fb1b368254bd6b2e03031828bb7199
Leah Miller <lkmiller.87@gmail.com>
2019-02-13 15:29:09 -0700

    Adds dependent destroy for plants in garden

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,6 +1,6 @@
 class Garden < ApplicationRecord
   belongs_to :user
-  has_many :plants
+  has_many :plants, dependent: :destroy
   validates_presence_of :name, :zip_code
   accepts_nested_attributes_for :plants, reject_if: :all_blank
 end

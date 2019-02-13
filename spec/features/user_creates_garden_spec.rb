d5fe85083f3fb70d50153f7b474814a60250c0a1
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-02-12 17:48:32 -0700

    Adds test for creating a garden.
    Co-authored-by: Anna Smolentzov 
    <6686861+asmolentzov@users.noreply.github.com>

--- spec/features/user_creates_garden_spec.rb
+++ spec/features/user_creates_garden_spec.rb
@@ -11,15 +11,40 @@ describe 'As a logged in user on the site' do
     expect(current_path).to eq(new_garden_path)
     expect(page).to have_field("Garden Name")
     expect(page).to have_field("Zip code")
+    expect(page).to have_field("Plant Name")
+    expect(page).to have_field("Plant Watering Requirements")
   end
 
-  it 'Can add plants on the new garden form' do
+  it 'Can create a garden' do
     user = User.create(name: "User1", email: "user@example.com")
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     visit new_garden_path
 
-    expect(page).to have_field("Plant Name")
-    expect(page).to have_field("Plant Watering Requirements")
+    fill_in :garden_name, with: "My Garden"
+    fill_in :garden_zip_code, with: "80203"
+    fill_in :garden_plants_name, with: "My Plant: Fuzzy"
+    fill_in :garden_plants_times_per_week, with: 5
+
+    fill_in :garden_plants_name, with: "Roses"
+    fill_in :garden_plants_times_per_week, with: 3
+
+    fill_in :garden_plants_name, with: "Sunflowers"
+    fill_in :garden_plants_times_per_week, with: 2
+
+    click_button "Create Garden"
+    garden = Garden.last
+
+    expect(current_path).to eq(garden_path(garden))
+    expect(page).to have_content(garden.name)
+    expect(page).to have_content(garden.zip_code)
+    within ".plants" do
+      expect(page).to have_content("Roses")
+      expect(page).to have_content("Watering Requirements: 3 times/week")
+      expect(page).to have_content("Sunflowers")
+      expect(page).to have_content("Watering Requirements: 2 times/week")
+      expect(page).to have_content("My Plant: Fuzzy")
+      expect(page).to have_content("Watering Requirements: 5 times/week")
+    end
   end
 end

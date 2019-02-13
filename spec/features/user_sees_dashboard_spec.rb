f9dbfb204472db154854a7493d70b700685de96a
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-02-13 15:28:13 -0700

    Adds specificity to testing. Adds links to show page.

--- spec/features/user_sees_dashboard_spec.rb
+++ spec/features/user_sees_dashboard_spec.rb
@@ -10,6 +10,7 @@ describe 'As a logged-in user, I see the dashboard' do
     expect(current_path).to eq(dashboard_path)
     expect(page).to have_content("Garden Dashboard")
     expect(page).to have_button("Create Garden")
+    expect(user.gardens.count).to eq(0)
     expect(page).to have_content("No Gardens Created Yet!")
   end
 
@@ -17,22 +18,28 @@ describe 'As a logged-in user, I see the dashboard' do
     user = create(:user)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
+    garden_1 = create(:garden, user: user)
+    garden_2 = create(:garden, user: user)
+    garden_3 = create(:garden, user: user)
+
     visit dashboard_path
 
     expect(current_path).to eq(dashboard_path)
     expect(page).to have_content("Garden Dashboard")
     expect(page).to have_button("Create Garden")
-    expect(gardens.count).to eq(3)
-    within ".garden-#{garden_1.id}" do
+    expect(user.gardens.count).to eq(3)
+
+    within "#garden-#{garden_1.id}" do
       expect(page).to have_content(garden_1.name)
+      expect(page).to have_content(garden_1.zip_code)
       expect(page).to have_link("#{garden_1.name}")
-      # expect(page).to have_selector(:css, 'a[href="/gardens/#{garden.id}"]')
-      # page.assert_selector(:link, nil, href: 'actual link')
     end
-    within ".garden-#{garden_2.id}" do
+
+    within "#garden-#{garden_2.id}" do
       expect(page).to have_content(garden_2.name)
+      expect(page).to have_content(garden_2.zip_code)
       click_on("#{garden_2.name}")
-      expect(current_path).to eq(gardens_path(garden_2))
+      expect(current_path).to eq(garden_path(garden_2))
     end
   end
 end

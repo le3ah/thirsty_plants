4447aeb9806914481b62d84caeca5b2e7abdead7
Leah Miller <lkmiller.87@gmail.com>
2019-02-13 08:33:32 -0700

    Adds flash message for create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,9 @@
 class SessionsController < ApplicationController
   def create
-    # binding.pry
     if user = User.from_google_auth(request.env["omniauth.auth"])
       session[:user_id] = user.id
+      flash[:success] = "Welcome, gardner, we're glad to have you among the roses."
+      redirect_to dashboard_path
     end
-    redirect_to dashboard_path
   end
 end

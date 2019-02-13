18bd8de9d37beb6667c4c49ef7dc5f3ad8fee71a
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-12 21:00:45 -0700

    Updates plants to be nested routes under gardens

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,6 +1,14 @@
 class PlantsController < ApplicationController
   
   def new
+    @garden = Garden.find(params[:garden_id])
     @plant = Plant.new
+  end
+  
+  def create
+    garden = Garden.find(params[:garden_id])
+    
+    flash[:success] = "Party in my plants, you've created a new plant!"
+    redirect_to garden_path(garden)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/dashboard', to: "users#show"
-  resources :gardens, only: [:new, :create, :show]
-  resources :plants, only: [:new, :create]
+  resources :gardens, only: [:new, :create, :show], shallow: true do
+    resources :plants, only: [:new, :create]
+  end
 end

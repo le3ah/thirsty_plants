573c2b88830c5410369b17b17486965df89fff9e
Leah Miller <lkmiller.87@gmail.com>
2019-02-13 08:33:15 -0700

    Adds validations in the user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
+validates_presence_of :first_name,
+                      :last_name,
+                      :email,
+                      :google_token,
+                      :google_id_token
+
   def self.from_google_auth(auth_info)
     where(google_id_token: auth_info[:extra][:id_token]).first_or_create do |new_user|
-      # binding.pry
       new_user.google_token           = auth_info.credentials.token
       new_user.first_name             = auth_info.info.first_name
       new_user.last_name              = auth_info.info.last_name

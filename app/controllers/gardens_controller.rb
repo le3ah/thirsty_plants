e76a0bd102800f83c1c9be76eb6321a9f2537555
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-02-12 17:58:21 -0700

    Adds route and create method for garden.
    Co-authored-by: Anna Smolentzov 
    <6686861+asmolentzov@users.noreply.github.com>

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -3,4 +3,13 @@ class GardensController < ApplicationController
   def new
     @garden = Garden.new
   end
+
+  def create
+    garden = current_user.gardens.create!(garden_params)
+  end
+
+  private
+  def garden_params
+    params.require(:garden).permit(:name, :zip_code, :plants)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/dashboard', to: "users#show"
-  resources :gardens, only: [:new, :create]
+  resources :gardens, only: [:new, :create, :show]
 end

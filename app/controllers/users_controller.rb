310753090b4995d6d9a75ef1d76b6741d3030018
Leah Miller <lkmiller.87@gmail.com>
2019-02-14 12:20:11 -0700

    Adds next seven days method inside controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,18 @@
 class UsersController < ApplicationController
   def show
     @user = current_user
+    @next_seven_days = next_seven_days
+  end
+
+  private
+
+  def next_seven_days
+    days = []
+    i = 1
+    7.times do
+      days << (Time.now + i.days).strftime('%A')
+      i += 1
+    end
+    days
   end
 end

5a6ae2ae953e914215d44d351da408af223759b3
Leah Miller <lkmiller.87@gmail.com>
2019-02-14 16:01:47 -0700

    Removes whitespace and commented out code

--- app/facades/dashboard_facade.rb
+++ app/facades/dashboard_facade.rb
@@ -3,20 +3,7 @@ class DashboardFacade
     @user = user
   end
 
-  # def chance_of_rain(index, lat, long)
-  #   raw_weather = dark_sky_service.get_weather(lat, long)
-  #   raw_day = raw_weather[:daily][:data][index]
-  #   raw_day[:precipProbability] * 100
-  # end
-
-  # def chance_of_rain(index, lat, long)
-  #   data = dark_sky_service.get_weather(lat, long)
-  #   Weather.new(data, index)
-  # end
-
   def weather(lat, long, index)
     Weather.new(lat, long, index)
-  end
-
-  
+  end  
 end

f95d414969f7d4e4a30d39f670622d3775e92618
Leah Miller <lkmiller.87@gmail.com>
2019-02-14 16:22:15 -0700

    Adds expectations to weather spec model test

--- spec/models/weather_spec.rb
+++ spec/models/weather_spec.rb
@@ -5,7 +5,7 @@ describe Weather do
     index = 0
     lat = "123.00005"
     long = "-0.123496"
-    weather = Weather.new(lat, long, index)
+    weather = Weather.new(lat, long)
 
     expect(weather).to be_a(Weather)
   end
@@ -14,7 +14,7 @@ describe Weather do
     long = "-0.123496"
     index = 0
 
-    weather = Weather.new(lat, long, index)
+    weather = Weather.new(lat, long)
     expect(weather.lat).to eq("123.00005")
     expect(weather.long).to eq("-0.123496")
   end

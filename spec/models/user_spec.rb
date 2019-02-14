9aab3687912902308fa4edc799dc444809bb7fa8
abenetka <ali.benetka@gmail.com>
2019-02-13 22:34:10 -0700

    Removes last_name validation test after oauth issue

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'validations' do
     it { should validate_presence_of :first_name }
-    it { should validate_presence_of :last_name }
     it { should validate_presence_of :email }
     it { should validate_presence_of :google_token }
     it { should validate_presence_of :google_id_token }

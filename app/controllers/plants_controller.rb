f0fe78e8cd48070d7594f79a87245533ae83f2f4
abenetka <ali.benetka@gmail.com>
2019-02-13 22:33:50 -0700

    Adds edit and update actions for plants

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,10 +1,10 @@
 class PlantsController < ApplicationController
-  
+
   def new
     @garden = Garden.find(params[:garden_id])
     @plant = Plant.new
   end
-  
+
   def create
     @garden = Garden.find(params[:garden_id])
     @plant = @garden.plants.new(plant_params)
@@ -16,7 +16,25 @@ class PlantsController < ApplicationController
       render :new
     end
   end
-  
+
+  def edit
+    @plant = Plant.find(params[:id])
+  end
+
+  def update
+    @plant = Plant.find(params[:id])
+    @garden = Garden.find(@plant.garden_id)
+    @plant.update(plant_params)
+    if @plant.save
+      flash[:success] = 'Plant updated successfully!'
+      redirect_to garden_path(@garden)
+    else
+      @errors = @plant.errors
+      render :edit
+    end
+  end
+
+
   def destroy
     plant = Plant.find(params[:id])
     garden = plant.garden
@@ -24,9 +42,9 @@ class PlantsController < ApplicationController
     flash[:success] = 'Goodbye dear plant'
     redirect_to garden_path(garden)
   end
-  
+
   private
-  
+
   def plant_params
     params.require(:plant).permit(:name, :times_per_week)
   end

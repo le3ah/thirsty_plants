32c88dd361e4e23daf2c098d7919ed4a622834ad
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-14 20:38:27 -0700

    Adds migration for zipcode

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190215012818) do
+ActiveRecord::Schema.define(version: 20190215032805) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,14 @@ ActiveRecord::Schema.define(version: 20190215012818) do
     t.datetime "updated_at", null: false
     t.float "latitude"
     t.float "longitude"
+  end
+
+  create_table "zipcodes", force: :cascade do |t|
+    t.string "zip_code"
+    t.string "latitude"
+    t.string "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "gardens", "users"

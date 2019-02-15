790fe24135d837b4d1288f8175c4f7d01a5dc305
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-14 18:31:28 -0700

    Adds lat/long validation to garden model, adds lat/long to garden factory

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,6 +1,6 @@
 class Garden < ApplicationRecord
   belongs_to :user
   has_many :plants, dependent: :destroy
-  validates_presence_of :name, :zip_code
+  validates_presence_of :name, :zip_code, :lat, :long
   accepts_nested_attributes_for :plants, reject_if: :all_blank
 end
--- spec/factories/gardens.rb
+++ spec/factories/gardens.rb
@@ -2,6 +2,8 @@ FactoryBot.define do
   factory :garden do
     sequence(:zip_code) { |n| "8020#{n.to_s}" }
     sequence(:name) { |n| "Front Yard#{n}" }
+    sequence(:lat) { |n| "12.#{n}"}
+    sequence(:long) { |n| "-12.#{n}"}
     user
   end
 end

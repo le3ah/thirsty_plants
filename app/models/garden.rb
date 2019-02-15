4691d81e582b5b5fda17c4049de602739bcb32b2
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-14 20:13:55 -0700

    Refactors lat_long callback

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -4,18 +4,16 @@ class Garden < ApplicationRecord
   validates_presence_of :name, :zip_code
   accepts_nested_attributes_for :plants, reject_if: :all_blank
 
-  before_save :set_lat_long
+  before_save :set_lat_long, unless: :has_lat_long?
   
   private
   
+  def has_lat_long?
+    self.lat && self.long
+  end
+  
   def set_lat_long
-    if self.lat && self.long
-      return
-    elsif self.zip_code != ''
-      self.lat = LocationService.new(self.zip_code).get_latitude
-      self.long = LocationService.new(self.zip_code).get_longitude
-    else
-      return
-    end
+    self.lat = LocationService.new(self.zip_code).get_latitude
+    self.long = LocationService.new(self.zip_code).get_longitude
   end
 end

3d2ad5c56c4d424581471ba3556c7650c5786519
Leah Miller <lkmiller.87@gmail.com>
2019-02-14 21:58:26 -0700

    Adds tests for remaining pages that a logged in user cannot see

--- spec/features/visitors/visitor_cannot_see_page_without_login_spec.rb
+++ spec/features/visitors/visitor_cannot_see_page_without_login_spec.rb
@@ -2,7 +2,29 @@ require 'rails_helper'
 
 describe 'as a visitor' do
   it "should not be able to see webpages if not logged in" do
+    garden = create(:garden)
+
     visit dashboard_path
+    expect(page).to have_content("The page you were looking for doesn't exist.")
+    expect(page.status_code).to eq(404)
+
+    visit new_garden_plant_path(garden)
+    expect(page).to have_content("The page you were looking for doesn't exist.")
+    expect(page.status_code).to eq(404)
+
+    visit new_garden_plant_path(garden)
+    expect(page).to have_content("The page you were looking for doesn't exist.")
+    expect(page.status_code).to eq(404)
+
+    visit new_garden_path
+    expect(page).to have_content("The page you were looking for doesn't exist.")
+    expect(page.status_code).to eq(404)
+
+    visit edit_garden_path(garden)
+    expect(page).to have_content("The page you were looking for doesn't exist.")
+    expect(page.status_code).to eq(404)
+
+    visit garden_path(garden)
     expect(page).to have_content("The page you were looking for doesn't exist.")
     expect(page.status_code).to eq(404)
 

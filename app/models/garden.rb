c17d8fa42044ab35cd0376ea588e0dd2e4398645
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-14 18:47:28 -0700

    Adds callback method to Garden

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -3,4 +3,19 @@ class Garden < ApplicationRecord
   has_many :plants, dependent: :destroy
   validates_presence_of :name, :zip_code, :lat, :long
   accepts_nested_attributes_for :plants, reject_if: :all_blank
+  
+  before_validation :set_lat_long
+  
+  private
+  
+  def set_lat_long
+    if self.lat && self.long
+      return
+    elsif self.zip_code
+      self.lat = LocationService.get_latitude(self.zip_code)
+      self.long = LocationService.get_longitude(self.zip_code)
+    else
+      return
+    end
+  end
 end

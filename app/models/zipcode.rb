c29374131a03eec9052b05934d3793209ec87018
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-14 20:47:02 -0700

    Adds validations and testing to zipcode model

--- app/models/zipcode.rb
+++ app/models/zipcode.rb
@@ -1,2 +1,3 @@
 class Zipcode < ApplicationRecord
+  validates_presence_of :zip_code, :latitude, :longitude
 end
--- spec/models/zipcode_spec.rb
+++ spec/models/zipcode_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Zipcode, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:zip_code) }
+  it { should validate_presence_of(:latitude) }
+  it { should validate_presence_of(:longitude) }
 end

9bea9189ff6453ece85d104d55245e28c353d3ce
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-14 18:50:39 -0700

    Adds location service test

---
+++ spec/services/location_service_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe LocationService do
+  it 'returns the latitude and longitude of a zip code' do
+    zip = 80203
+    lat = '39.7312095'
+    long = '-104.9826965'
+    user = create(:user)
+    garden = user.gardens.create(name: 'Garden', zip_code: zip)
+    
+    expect(garden.lat).to eq(lat)
+    expect(garden.long).to eq(long)
+  end
+end

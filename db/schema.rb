5c77553c0f1372a9695e340b1ba79a4cfd4804c7
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-14 18:29:01 -0700

    Adds migration for adding lat/long to garden

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190213010824) do
+ActiveRecord::Schema.define(version: 20190215012818) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20190213010824) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "lat"
+    t.string "long"
     t.index ["user_id"], name: "index_gardens_on_user_id"
   end
 
@@ -41,6 +43,8 @@ ActiveRecord::Schema.define(version: 20190213010824) do
     t.string "google_id_token"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   add_foreign_key "gardens", "users"

1d9a207b9d51ed42786ee73263dc529903ee6942
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-02-15 18:26:41 -0700

    Adds files copied from slack.

---
+++ spec/features/users/user_can_see_nav_bar_spec.rb
@@ -0,0 +1,73 @@
+require 'rails_helper'
+
+describe 'As a user to every page except the welcome page' do
+  it 'Cannot see the navbar on welcome page' do
+    visit root_path
+
+      expect(page).to_not have_link('Dashboard')
+      expect(page).to_not have_link('My Gardens')
+      expect(page).to_not have_link('Schedule')
+      expect(page).to_not have_link('Sign Out')
+  end
+
+  it 'Navbar links work' do
+    user = create(:user)
+    garden = create(:garden, user: user)
+    plant = create(:plant, garden: garden)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit dashboard_path
+
+    within '.navbar' do
+      click_on ('Dashboard')
+      expect(current_path).to eq(dashboard_path)
+    end
+
+    within '.navbar' do
+      click_on('My Gardens')
+      expect(current_path).to eq(garden_path(garden))
+    end
+  end
+
+  xit 'Can see the navbar on all pages (except welcome)' do
+    user = create(:user)
+    garden = create(:garden, user: user)
+    plant = create(:plant, garden: garden)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit dashboard_path
+
+    within '.navbar' do
+      expect(page).to have_link('Dashboard')
+      expect(page).to have_link('My Gardens')
+    end
+
+    visit new_garden_plant_path(garden)
+
+    within '.navbar' do
+      expect(page).to have_link('Dashboard')
+      expect(page).to have_link('My Gardens')
+    end
+
+    visit new_garden_path
+
+    within '.navbar' do
+      expect(page).to have_link('Dashboard')
+      expect(page).to have_link('My Gardens')
+    end
+
+    visit edit_garden_path(garden)
+
+    within '.navbar' do
+      expect(page).to have_link('Dashboard')
+      expect(page).to have_link('My Gardens')
+    end
+
+    visit garden_path(garden)
+
+    within '.navbar' do
+      expect(page).to have_link('Dashboard')
+      expect(page).to have_link('My Gardens')
+    end
+  end
+end

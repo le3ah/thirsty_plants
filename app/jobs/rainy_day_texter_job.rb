4fa8a005d3671978e85e58f24b982441c4144f41
Ben Lee <bendelonlee@gmail.com>
2019-02-17 15:09:26 -0700

    Adds a method to rainy day texter that sends a text each morning

--- app/jobs/rainy_day_texter_job.rb
+++ app/jobs/rainy_day_texter_job.rb
@@ -1,7 +1,30 @@
 class RainyDayTexterJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform
+    users_with_phone_numbers.each do |user|
+      send_rainy_day_text(user)
+    end
+    RainyDayTexterJob.set(wait_until: early_next_morning).perform_later
+  end
+
+  def early_next_morning
+    1.day.from_now.beginning_of_day + 5.hours
+  end
+
+  def self.early_next_morning
+    early_next_morning
+  end
+
+  def users_with_phone_numbers
+    User.where.not(telephone: nil)
+  end
+
+  def send_rainy_day_text(user, chance = 110)
+    MyTwillioClient.api.account.messages.create(
+      from: '+12028834286',
+      to: "+1#{user.telephone}",
+      body: "Heads up from thirsty plants! There is a #{chance}% chance of precipitation in your area today."
+    )
   end
 end

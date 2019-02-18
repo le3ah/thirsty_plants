77020d1a085ca0516cb384f504e2819c2e1fe498
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-18 12:10:30 -0700

    Adds numericality and range validation for plant waterings per week

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -3,4 +3,7 @@ class Plant < ApplicationRecord
   has_many :waterings, dependent: :destroy
   validates_presence_of :name
   validates_presence_of :times_per_week
+  validates_numericality_of :times_per_week,
+                            greater_than_or_equal_to: 0, 
+                            less_than_or_equal_to: 35
 end

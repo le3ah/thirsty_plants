df196c89ff1f3500b0218d431a39ef9e9f88adfb
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-17 20:58:15 -0700

    Adds feature test for still seeing Garden Index page even if weather API calls fail

--- spec/features/users/user_can_still_see_pages_if_api_calls_fail_spec.rb
+++ spec/features/users/user_can_still_see_pages_if_api_calls_fail_spec.rb
@@ -16,4 +16,20 @@ describe 'As a logged in user of the site' do
       expect(page).to have_content('Garden Dashboard')
     end
   end
+  
+  describe 'on the gardens index page' do
+    it 'can see the site even if API calls fail' do
+      user = create(:user)
+      garden = create(:garden, user: user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      allow_any_instance_of(DarkSkyService).to receive(:get_weather).and_return(nil)
+      
+      visit gardens_path
+      
+      expect(page.status_code).to eq(200)
+      expect(page).to_not have_content('Weather')
+      expect(page).to_not have_css('weather_day')
+      expect(page).to have_content(garden.name)
+    end
+  end
 end

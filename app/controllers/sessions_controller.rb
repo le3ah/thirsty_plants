a553470860a68b54033427fa3f2a3c3a463aa3e4
abenetka <ali.benetka@gmail.com>
2019-02-17 17:02:16 -0700

    Updates sessions create for different roles

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,20 @@
 class SessionsController < ApplicationController
   skip_before_action :require_login
-  
+
   def create
-    if user = User.from_google_auth(request.env["omniauth.auth"])
+    user = User.from_google_auth(request.env["omniauth.auth"])
+    if user && user.role == "default"
       session[:user_id] = user.id
       flash[:success] = "Welcome, gardener, we're glad to have you among the roses."
       redirect_to dashboard_path
+    else
+      user && user.role == "admin"
+      session[:user_id] = user.id
+      flash[:success] = "Welcome, Admin!"
+      redirect_to admin_dashboard_path
     end
   end
-  
+
   def destroy
     session[:user_id] = nil
     redirect_to root_path

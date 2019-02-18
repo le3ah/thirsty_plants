7ab291b5cd092e4c673495171b3c9538fd987b12
Ben Lee <bendelonlee@gmail.com>
2019-02-18 10:02:27 -0700

    Adds user with my phone number to the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+include FactoryBot::Syntax::Methods
+create(:user, telephone: ADMIN_PHONE_NUMBER)

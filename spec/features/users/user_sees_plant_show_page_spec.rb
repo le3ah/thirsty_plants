43d7710bc94a57698f79398619fffe6bafbf5fa2
Leah Miller <lkmiller.87@gmail.com>
2019-02-18 16:38:39 -0700

    Adds feature tests for plant show page

---
+++ spec/features/users/user_sees_plant_show_page_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+describe  'as a logged-in user' do
+  it "can see plant show page from the garden show page" do
+    user = create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    garden = create(:garden)
+    plant = create(:plant, garden: garden)
+
+    visit garden_path(garden)
+
+    click_on "#{plant.name}"
+
+    expect(current_path).to eq(plant_path(plant))
+    expect(page).to have_content(plant.name)
+    expect(page).to have_content("#{plant.times_per_week} times per week")
+  end
+  it "can see plant show page from the garden show page" do
+    user = create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    garden = create(:garden, user: user)
+    plant = create(:plant, garden: garden)
+
+    visit gardens_path
+
+    click_link "#{plant.name}"
+
+    expect(current_path).to eq(plant_path(plant))
+    expect(page).to have_content(plant.name)
+    expect(page).to have_content(plant.name)
+    expect(page).to have_content("#{plant.times_per_week} times per week")
+  end
+end

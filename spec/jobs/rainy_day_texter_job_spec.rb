1177297dfd3e1f368a366a64fc0805ab9e08a7c2
abenetka <ali.benetka@gmail.com>
2019-02-19 16:30:54 -0700

    Updates test

--- spec/jobs/rainy_day_texter_job_spec.rb
+++ spec/jobs/rainy_day_texter_job_spec.rb
@@ -1,20 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe RainyDayTexterJob, type: :job do
+RSpec.describe RainyDayJob, type: :job do
   before(:each) do
     ActiveJob::Base.queue_adapter = :test
   end
 
   it "exists", :vcr, :allow_playback_repeats => true do
     expect {
-      RainyDayTexterJob.perform_later
-    }.to have_enqueued_job(RainyDayTexterJob)
+      RainyDayJob.perform_later
+    }.to have_enqueued_job(RainyDayJob)
   end
 
   it 'reschedules itself for the next morning', :vcr, :allow_playback_repeats => true do
     expect {
-      RainyDayTexterJob.perform_now
-    }.to have_enqueued_job(RainyDayTexterJob).at(RainyDayTexterJob.early_next_morning)
+      RainyDayJob.perform_now
+    }.to have_enqueued_job(RainyDayJob).at(RainyDayJob.early_next_morning)
   end
 
 end

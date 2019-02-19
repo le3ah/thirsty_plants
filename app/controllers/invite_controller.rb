aeefb4eae5199f55a9088ab1897d4a6c7d098059
abenetka <ali.benetka@gmail.com>
2019-02-18 20:25:51 -0700

    Adds create action to invite controller

--- app/controllers/invite_controller.rb
+++ app/controllers/invite_controller.rb
@@ -2,4 +2,10 @@ class InviteController < ApplicationController
   def show
   end
 
+  def create
+    CaretakerNotifierMailer.inform(current_user, params[:email]).deliver_now
+    flash[:notice] = "Success! You invited a caretaker to watch your gardens."
+    redirect_to dashboard_url
+  end
+
 end

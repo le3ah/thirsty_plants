e80fa588b4e0f16af9973227ee193443d76abcf7
Leah Miller <lkmiller.87@gmail.com>
2019-02-20 15:43:57 -0700

    Removes validation requirement of thumbnail image

--- spec/models/plant_spec.rb
+++ spec/models/plant_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe Plant, type: :model do
   describe 'Validations' do
     it { should validate_presence_of :name}
     it { should validate_presence_of :times_per_week}
-    # it { should validate_attachment_presence :thumbnail}
     it { should validate_numericality_of(:times_per_week).is_greater_than_or_equal_to(0) }
     it { should validate_numericality_of(:times_per_week).is_less_than_or_equal_to(35) }
   end

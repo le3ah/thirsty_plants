aa1824f4f498af0d47c7dcfee9caaabf87714f5f
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-19 18:41:33 -0700

    Adds feature test for updating a plant waterings per week

--- spec/features/users/user_generates_schedule_when_adding_a_plant_spec.rb
+++ spec/features/users/user_generates_schedule_when_adding_a_plant_spec.rb
@@ -50,4 +50,45 @@ describe 'As a logged in user when I add a plant' do
       expect(page).to_not have_content(plant.name)
     end
   end
+  
+  it 'updates the schedule when you update the times per week for a plant' do
+    user = create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    garden = create(:garden, user: user)
+    plant = create(:plant, garden: garden, times_per_week: 3)
+    
+    visit edit_plant_path(plant)
+    
+    fill_in :plant_times_per_week, with: 7
+    click_button 'Update Plant'
+    
+    expect(plant.reload.times_per_week).to eq(7)
+    
+    visit schedules_path
+    
+    within("div[name='#{Date.today.strftime('%b%d')}']") do
+      expect(page).to have_content(plant.name, count: 1)
+    end
+    within("div[name='#{1.days.from_now.localtime.strftime('%b%d')}']") do
+      expect(page).to have_content(plant.name, count: 1)
+    end
+    within("div[name='#{2.days.from_now.localtime.strftime('%b%d')}']") do
+      expect(page).to have_content(plant.name, count: 1)
+    end
+    within("div[name='#{3.days.from_now.localtime.strftime('%b%d')}']") do
+      expect(page).to have_content(plant.name, count: 1)
+    end
+    within("div[name='#{4.days.from_now.localtime.strftime('%b%d')}']") do
+      expect(page).to have_content(plant.name, count: 1)
+    end
+    within("div[name='#{5.days.from_now.localtime.strftime('%b%d')}']") do
+      expect(page).to have_content(plant.name, count: 1)
+    end
+    within("div[name='#{6.days.from_now.localtime.strftime('%b%d')}']") do
+      expect(page).to have_content(plant.name, count: 1)
+    end
+    within("div[name='#{7.days.from_now.localtime.strftime('%b%d')}']") do
+      expect(page).to have_content(plant.name, count: 1)
+    end
+  end
 end

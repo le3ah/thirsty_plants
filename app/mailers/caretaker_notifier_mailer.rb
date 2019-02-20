51ce0f4691109b31fe6c84749714343b94b658c8
Ben Lee <bendelonlee@gmail.com>
2019-02-20 15:42:42 -0700

    Adds link to email

--- app/mailers/caretaker_notifier_mailer.rb
+++ app/mailers/caretaker_notifier_mailer.rb
@@ -1,7 +1,7 @@
 class CaretakerNotifierMailer < ApplicationMailer
-  def inform(user, friend_contact)
+  def inform(user, friend_contact, garden)
     @user = user
-    mail(to: friend_contact, subject: "#{user.first_name} wants you to check out their gardens!")
+    mail(to: friend_contact, subject: "#{user.first_name} wants you to caretake their gardens!")
   end
 
 end

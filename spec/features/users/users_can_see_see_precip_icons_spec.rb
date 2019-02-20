7803a0847edf3f9da288a3abd55738c52b9f2076
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-02-19 19:47:37 -0700

    Refines specificity of test.

--- spec/features/users/users_can_see_see_precip_icons_spec.rb
+++ spec/features/users/users_can_see_see_precip_icons_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 describe "As a user, visiting the site" do
   it "sees icons is accordance with weather conditions", :vcr do
+    user = create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
     service = DarkSkyService.new
     lat = "1.43533"
     long = "-0.0004503993"
@@ -11,18 +14,27 @@ describe "As a user, visiting the site" do
     precip_type = weather_day[:precipType]
     day = Time.now.strftime('%A')
 
+    visit gardens_path
+    expect(page).to_not have_content("Chance of #{precip_type}")
+
+    visit dashboard_path
+    expect(page).to_not have_content("Chance of #{precip_type}")
+
+    garden = create(:garden, user: user)
+    plant = create(:plant, garden: garden)
+
     visit dashboard_path
 
-    within ".weather_day#{day}" do
-      expect(page).to have_css("img[src*='environ-peeling-kuur.jpg']")
-      expect(page).to have_content("Chance of #{precip_type}")
+    within ".gardens-container" do
+      # expect(page).to have_css("img[src*='environ-peeling-kuur.jpg']")
+      expect(page).to have_content("#{day}, Chance of #{precip_type.capitalize}")
     end
 
     visit gardens_path
 
-    within ".weather_day#{day}" do
-      expect(page).to have_image()
-      expect(page).to have_content("Chance of #{precip_type}")
+    within ".gardens-container" do
+      # expect(page).to have_image()
+      expect(page).to have_content("#{day}, Chance of #{precip_type.capitalize}")
     end
   end
 end

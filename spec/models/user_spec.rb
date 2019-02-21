31be8b03f9908879c7160344f8996d0f1e580484
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-21 11:08:14 -0700

    Adds model test for user own gardens

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,4 +11,20 @@ RSpec.describe User, type: :model do
   describe 'Relationships' do
     it { should have_many(:gardens) }
   end
+  
+  describe 'Instance Methods' do
+    describe '#own_gardens' do
+      it 'should return the gardens for which the user is the owner' do
+        owner = create(:user)
+        garden_1 = create(:garden, owners: [owner])
+        garden_2 = create(:garden, owners: [owner])
+        not_owner = create(:user)
+        garden_3 = create(:garden, owners: [not_owner])
+        create(:user_garden, garden: garden_3, user: owner, relationship_type: 'caretaker')
+        
+        expect(owner.own_gardens).to eq([garden_1, garden_2])
+        expect(not_owner.own_gardens).to eq([garden_3])
+      end
+    end
+  end
 end

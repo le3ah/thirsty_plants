fbdeed29b07f0d45b913246c51dbb2fbbca3dcb5
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-20 20:27:12 -0700

    Adds get_all_weather_data Weather class method

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -6,6 +6,16 @@ attr_reader :lat, :long, :garden
     @lat = garden.lat
     @long = garden.long
   end
+  
+  def self.get_all_weather_data
+    Garden.all.each do |garden|
+      garden.update(weather_data: dark_sky_service.get_weather(garden.lat, garden.long))
+    end
+  end
+  
+  def self.dark_sky_service
+    @_dark_sky_service ||= DarkSkyService.new
+  end
 
   def chance_of_rain(day_index)
     raw_day = weather_info["daily"]["data"][day_index]

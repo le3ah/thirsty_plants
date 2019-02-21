6508fa9dfa1adac2cbc7bcd3da587f06109058c7
Ben Lee <bendelonlee@gmail.com>
2019-02-21 16:42:08 -0700

    Removes unused methods

--- app/jobs/rainy_day_job.rb
+++ app/jobs/rainy_day_job.rb
@@ -15,18 +15,6 @@ class RainyDayJob < ApplicationJob
     @num_texts
   end
 
-  def early_next_morning
-    self.class.early_next_morning
-  end
-
-  def self.early_next_morning
-    Time.use_zone("Mountain Time (US & Canada)") { 1.day.from_now.beginning_of_day + 5.hours }
-  end
-
-  def users_with_phone_numbers
-    User.where.not(telephone: nil)
-  end
-
   def send_rainy_day_texts(rainy_day)
     rainy_day.gardens.each do |garden|
       garden.users.each do |user|

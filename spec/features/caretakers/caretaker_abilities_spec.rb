58213f12780a92c3d4f6b4271e2a57757aa47f60
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-21 16:24:26 -0700

    Adds feature test for not being able to add other caretakers

--- spec/features/caretakers/caretaker_abilities_spec.rb
+++ spec/features/caretakers/caretaker_abilities_spec.rb
@@ -52,4 +52,19 @@ describe 'As a caretaker of a garden' do
     visit edit_plant_path(plant)
     expect(status_code).to eq(404)
   end
+  it 'cannot invite other caretakers to the garden' do
+    owner = create(:user)
+    caretaker = create(:user, first_name: 'Caretaker')
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(caretaker)
+    garden = create(:garden, owners: [owner])
+    plant = create(:plant, garden: garden)
+    create(:user_garden, garden: garden, user: caretaker, relationship_type: 'caretaker')
+    own_garden = create(:garden, owners: [caretaker])
+    
+    visit gardens_path
+    
+    within('.caretaking-gardens') do
+      expect(page).to_not have_link('Add Caretaker for this Garden')
+    end
+  end
 end

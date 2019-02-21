6691a7662c3bd807253ea29576e2b888dfc5e828
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-20 21:57:53 -0700

    Removes commented out line

--- app/models/rainy_day.rb
+++ app/models/rainy_day.rb
@@ -11,7 +11,6 @@ class RainyDay
 
   def self.generate_rainy_days
     gardens_to_check_weather_for.map do | garden |
-      # zip_code_finder = ZipcodeFinder.new(zip_code)
       weather = Weather.new(garden)
       if (chance = weather.chance_of_rain(0)) > 50
         RainyDay.new(chance_of_rain: chance, zip_code: garden.zip_code)

875a96937bb53c1dae9f4832a72f69406e17b8dc
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-02-21 11:45:25 -0700

    Removes comments (old tests).

--- spec/features/users/users_can_see_see_precip_icons_spec.rb
+++ spec/features/users/users_can_see_see_precip_icons_spec.rb
@@ -1,57 +1,6 @@
 require 'rails_helper'
 
 describe "As a user, visiting the site" do
-  # it "sees icons is accordance with weather conditions", :vcr do
-  #   user = create(:user)
-  #   allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-  #
-  #   service = DarkSkyService.new
-  #   lat = "1.43533"
-  #   long = "-0.0004503993"
-  #
-  #   weather_info = service.get_weather(lat, long)
-  #   weather_day = weather_info[:daily][:data][0]
-  #   if weather_day[:precipType]
-  #     precip_type = weather_day[:precipType]
-  #   else
-  #     precip_type = "precipitation"
-  #   end
-  #
-  #   today = (Time.now).strftime('%A')
-  #
-  #   visit gardens_path
-  #   expect(page).to_not have_content("Chance of #{precip_type}")
-  #
-  #   visit dashboard_path
-  #   expect(page).to_not have_content("Chance of #{precip_type}")
-  #
-  #   garden = create(:garden, user: user)
-  #   plant = create(:plant, garden: garden)
-  #
-  #   if weather_day[:precipType].downcase == "snow"
-  #     selector = ".fa-snowflakes"
-  #   elsif weather_day[:precipType].downcase == "sleet"
-  #     selctor = ".fa-cloud-sleet"
-  #   elsif weather_day[:precipType] == nil
-  #     selector = ".fa-raindrops"
-  #   else
-  #     selector = ".fa-raindrops"
-  #   end
-  #   visit dashboard_path
-  #
-  #   within ".gardens-container" do
-  #     expect(page).to have_selector(selector)
-  #     expect(page).to have_content("#{today}, Chance of #{precip_type.capitalize}")
-  #   end
-  #
-  #   visit gardens_path
-  #
-  #   within ".gardens-container" do
-  #     # expect(page).to have_selector(selector)
-  #     # expect(page).to have_content("#{today}, Chance of #{precip_type.capitalize}")
-  #   end
-  # end
-
   it "sees icons is accordance with weather conditions" do
     user = create(:user)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
@@ -92,7 +41,3 @@ describe "As a user, visiting the site" do
     expect(precip_icon).to eq("far fa-snowflake")
   end
 end
-
-# <% if @facade.weather(garden.lat, garden.long).chance_of_rain(index).round(0) >= 40 %>
-#   <i class="<%= @facade.weather(garden.lat, garden.long).precip_icon(index) %>"></i>
-# <% end %>

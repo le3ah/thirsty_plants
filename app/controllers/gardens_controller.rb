8cba9983c0fc98aec3ada4cff441bf064b78d4dc
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-21 15:54:52 -0700

    Updates gardens controller with before action for requiring users be owners to update gardens

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -1,5 +1,6 @@
 class GardensController < ApplicationController
-
+  before_action :require_garden_owner, only: [:edit, :update, :destroy]
+  
   def new
     @garden = Garden.new
     3.times { @garden.plants.build }
@@ -18,8 +19,7 @@ class GardensController < ApplicationController
   end
 
   def show
-    @garden = Garden.find(params[:id])
-    unless @garden.users.include? current_user
+    unless garden.users.include? current_user
       render_404
     end
   end
@@ -31,29 +31,32 @@ class GardensController < ApplicationController
   end
 
   def edit
-    @garden = Garden.find(params[:id])
+    @garden = garden
   end
 
   def update
-    @garden = Garden.find(params[:id])
-    @garden.update(garden_params)
-    if @garden.save
+    garden.update(garden_params)
+    if garden.save
       flash[:success] = 'Garden updated successfully!'
-      redirect_to garden_path(@garden)
+      redirect_to garden_path(garden)
     else
-      @errors = @garden.errors
+      @errors = garden.errors
       render :edit
     end
   end
 
   def destroy
-    garden = Garden.find(params[:id])
     garden.destroy
     flash[:success] = "Garden successfully deleted."
     redirect_to dashboard_path
   end
 
   private
+  
+  def garden
+    @garden ||= Garden.find(params[:id])
+  end
+  
   def garden_params
     params.require(:garden)
     .permit(:name, :zip_code, :plants,
@@ -69,5 +72,8 @@ class GardensController < ApplicationController
     end
     days
   end
-
+  
+  def require_garden_owner
+    render_404 unless current_user.own_gardens.include?(garden)
+  end
 end

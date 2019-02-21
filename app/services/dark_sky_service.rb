fb1f38412f23b2645e8d3df4b6d745da78b314bb
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-02-20 17:30:15 -0700

    Removes pry.

--- app/services/dark_sky_service.rb
+++ app/services/dark_sky_service.rb
@@ -3,7 +3,6 @@ class DarkSkyService
   def get_weather(lat, long)
     Rails.cache.fetch("weather_info_#{lat}_#{long}", expires_in: 1.hour) do
       response = conn.get("/forecast/#{ENV["darksky_api_secret"]}/#{lat},#{long}")
-      binding.pry
       JSON.parse(response.body, symbolize_names: true)
     end
   end

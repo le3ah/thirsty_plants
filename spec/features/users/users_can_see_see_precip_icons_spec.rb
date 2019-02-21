0c59c8b1cec6a2643d9057756bcb4b4387aa2bfe
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-02-21 09:36:31 -0700

    Adds day. Visits page. Expects content. Expects model methods.

--- spec/features/users/users_can_see_see_precip_icons_spec.rb
+++ spec/features/users/users_can_see_see_precip_icons_spec.rb
@@ -56,6 +56,7 @@ describe "As a user, visiting the site" do
     user = create(:user)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
     day_index = 0
+    today = (Time.now).strftime('%A')
     lat = "123.00005"
     long = "-0.123496"
     weather_info =   {"daily": {
@@ -72,20 +73,32 @@ describe "As a user, visiting the site" do
                 "precipType": "snow"
             }] }}
 
-
     weather = Weather.new(lat, long)
     allow(weather).to receive(:weather_info).and_return(weather_info)
     precip_type = weather.precip_type(day_index).capitalize
+    precip_icon = weather.precip_icon(day_index)
     expect(precip_type).to eq("Snow")
 
     visit gardens_path
     expect(page).to_not have_content("Chance of #{precip_type}")
-    #
-    #   visit dashboard_path
-    #   expect(page).to_not have_content("Chance of #{precip_type}")
-    #
 
-    #   garden = create(:garden, user: user)
-    #   plant = create(:plant, garden: garden)
+    visit dashboard_path
+    expect(page).to_not have_content("Chance of #{precip_type}")
+
+    garden = create(:garden, user: user)
+    plant = create(:plant, garden: garden)
+
+    visit dashboard_path
+
+    expect(precip_type).to eq("Snow")
+    expect(precip_icon).to eq("far fa-snowflakes")
+    save_and_open_page
+
+    within ".gardens-container" do
+      # expect(page).to have_selector(".fa-snowflakes")
+      expect(page).to have_content("#{today}, Chance of #{precip_type}")
+    end
+
+
   end
 end

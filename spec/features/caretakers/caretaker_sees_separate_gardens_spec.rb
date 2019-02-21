347077908b6c47559da96fd3283b6236accd4b90
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-21 11:39:52 -0700

    Adds feature test for not seeing caretaker section if not taking care of gardens

--- spec/features/caretakers/caretaker_sees_separate_gardens_spec.rb
+++ spec/features/caretakers/caretaker_sees_separate_gardens_spec.rb
@@ -24,5 +24,20 @@ describe 'As a caretaker' do
         expect(page).to_not have_content(own_garden.name)
       end
     end
+    it 'should not show caretaking section if they are not taking care of other gardens' do
+      owner = create(:user)
+      caretaker = create(:user, first_name: 'Caretaker')
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(owner)
+      garden = create(:garden, owners: [owner])
+      caretaker_garden = create(:garden, owners: [caretaker])
+      create(:user_garden, garden: garden, user: caretaker, relationship_type: 'caretaker')
+      
+      visit gardens_path
+      
+      expect(page).to have_content("#{owner.first_name}'s Greenery")
+      expect(page).to_not have_content('Gardens Under My Care')
+      expect(page).to_not have_content(caretaker_garden.name)
+      expect(page).to have_content(garden.name)
+    end
   end
 end

5dce9cbe355eab9e562adc5b7d5e89f270cfd643
Ben Lee <bendelonlee@gmail.com>
2019-03-01 17:28:33 -0700

    Implements changes to pass tests. Ended up needing to use some ruby (uniq). As is, this is not an ideal solution in terms of simplicity or database effeciency. Possible refactors: 1) add columns for chance_of_precipitation (as well as frost) so don't have to do a query, a check with the jsonb/ external api, then another query. Instead we could just do one query. 2) Consider moving the weather data from the gardens table to the zipcodes table so we are storing identical data.

--- app/jobs/rainy_day_job.rb
+++ app/jobs/rainy_day_job.rb
@@ -9,7 +9,7 @@ class RainyDayJob < ApplicationJob
 
   def text_users
     @num_texts = 0
-    RainyDay.generate_rainy_days.each do |rainy_day|
+    RainyDay.generate_rainy_days.uniq(&:zip_code).each do |rainy_day|
       send_rainy_day_texts(rainy_day)
     end
     @num_texts
@@ -17,10 +17,14 @@ class RainyDayJob < ApplicationJob
 
   def send_rainy_day_texts(rainy_day)
     rainy_day.gardens.each do |garden|
-      garden.users.where.not(telephone: nil).each do |user|
+      users_to_text(garden).each do |user|
         @num_texts += 1
         RainyDayTexter.send_rainy_day_text(user, garden, rainy_day.chance_of_rain)
       end
     end
+  end
+
+  def users_to_text(garden)
+    garden.users.where(receive_texts: true, rainy_day_notifications: true)
   end
 end

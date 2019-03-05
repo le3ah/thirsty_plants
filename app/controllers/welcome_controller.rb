00a6998065572a32c745adc5eb48eb2c2745bd51
Ben Lee <bendelonlee@gmail.com>
2019-03-05 08:38:18 -0700

    Redirects users to dashboard from root path if they are logged in

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,8 @@
 class WelcomeController < ApplicationController
   layout "welcome"
   skip_before_action :require_login, only: [:index]
-  
+
   def index
+    redirect_to dashboard_path if current_user
   end
 end
--- spec/features/users/user_logs_in_spec.rb
+++ spec/features/users/user_logs_in_spec.rb
@@ -11,5 +11,14 @@ describe 'subject' do
       click_link "Sign in with Google"
       expect(page).to have_content(garden.name)
     end
+
+  end
+  describe 'as a logged in user' do
+    it 'when I navigate to the root path I am redirected to my dashboard and not a link to log in' do
+      user = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      visit root_path
+      expect(current_path).to eq(dashboard_path)
+    end
   end
 end
